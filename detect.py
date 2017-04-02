065a3a41d15fd742061b62f4dce49e23e9446101
Kamil Kowalski <kowalski.tychy@gmail.com>
2017-04-03 00:35:52 +0200

    Add simple face tracking and unsetting

--- detect.py
+++ detect.py
@@ -11,6 +11,8 @@ import pyrebase
 # Setup some constants
 IMAGE_WIDTH = 512
 IMAGE_HEIGHT = 288
+TRACKING_THRESHOLD_X = IMAGE_WIDTH / 6
+TRACKING_THRESHOLD_Y = IMAGE_HEIGHT / 6
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_alt.xml".format(
         base_path=BASE_PATH)
@@ -37,6 +39,8 @@ sleep(2)
 print("Starting face detection, press Ctrl+C to exit")
 
 running = True
+last_center_x  = None
+last_center_y = None
 
 while running:
         try:
@@ -48,41 +52,59 @@ while running:
                 print("Running face classifier")
                 rects = face_cascade.detectMultiScale(image, scaleFactor=1.5, minNeighbors=5,
                         minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-
+                
                 if len(rects) > 0:
-                        print("Faces detected!")
+                        print("Processing detection")
+                        x, y, width, height = rects[0]
+
+                        center_x = x + width / 2
+                        center_y = y + height / 2
+
+                        if last_center_x == None or last_center_y == None:
+                                center_delta_x = IMAGE_WIDTH
+                                center_delta_y = IMAGE_HEIGHT
+                        else:
+                                center_delta_x = abs(last_center_x - center_x)
+                                center_delta_y = abs(last_center_y - center_y)
+
+                        last_center_x = center_x
+                        last_center_y = center_y
+
+                        if center_delta_x < TRACKING_THRESHOLD_X and center_delta_y < TRACKING_THRESHOLD_Y:
+                                print("Face already detected")
+                        else:
+                                print("Possible new face - attempting identification")
+                                cropped = image[y:y+height, x:x+width]
+                                impath = "{base_path}/detections/{imagename}.jpg".format(
+                                        base_path=BASE_PATH, imagename=strftime("%c"))
+                                cv2.imwrite(impath, cropped)
+
+                                print("Using CF to obtain a faceId")
+                                detections = CF.face.detect(impath)
+
+                                if len(detections) > 0:
+                                        print("Attemtping to identify face")
+                                        faceId = detections[0]["faceId"]
+                                        faces = CF.face.identify([faceId], PERSON_GROUP_ID)
+
+                                        if len(faces) > 0:
+                                                candidates = faces[0]["candidates"]
+                                                if len(candidates) > 0:
+                                                        personId = candidates[0]["personId"]
+
+                                                        print("Recognized person, writing to Firebase")
+                                                        db.child("screens").child(SCREEN_ID).child("personId").set(personId)
+                                                else:
+                                                        print("CF couldn't identify the face")
+                                                        db.child("screens").child(SCREEN_ID).child("personId").remove()
+                                else:
+                                        print("CF didn't detect a face")
+                                        db.child("screens").child(SCREEN_ID).child("personId").remove()
                 else:
                         print("No faces detected")
-
-                i = 0
-                for (x, y, width, height) in rects:
-                        print("Processing detection")
-                        cropped = image[y:y+height, x:x+width]
-                        impath = "{base_path}/detections/{imagename} ({no}).jpg".format(
-                                base_path=BASE_PATH, imagename=strftime("%c"), no=i)
-                        cv2.imwrite(impath, cropped)
-
-                        print("Using CF to obtain a faceId")
-                        detections = CF.face.detect(impath)
-
-                        if len(detections) > 0:
-                                print("Attemtping to identify face")
-                                faceId = detections[0]["faceId"]
-                                faces = CF.face.identify([faceId], PERSON_GROUP_ID)
-
-                                if len(faces) > 0:
-                                        candidates = faces[0]["candidates"]
-                                        if len(candidates) > 0:
-                                                personId = candidates[0]["personId"]
-
-                                                print("Recognized person, writing to Firebase")
-                                                db.child("screens").child(SCREEN_ID).child("personId").set(personId)
-                                        else:
-                                                print("CF couldn't identify the face")
-                        else:
-                                print("CF didn't detect a face")
-
-                        i+=1
+                        last_center_x = None
+                        last_center_y = None
+                        db.child("screens").child(SCREEN_ID).child("personId").remove()
 
                 print("Finishing loop run")
                 sleep(1)
