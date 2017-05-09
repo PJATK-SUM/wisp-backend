2e48724e6d06bf3f81d8a2c3ba8a01b89f866571
Kamil Kowalski <kowalski.tychy@gmail.com>
2017-05-10 01:20:45 +0200

    Implement fetching with requests library, add studentId fetch

--- libs/schedule.py
+++ libs/schedule.py
@@ -1,91 +1,104 @@
 # Contributed by Mateusz Knop (https://github.com/Persiasty)
 
-import urllib2
+import requests
 import json
 import libs.logger
+import secrets
+import xml.etree.ElementTree as ET
 from datetime import date, datetime
-from secrets import SCHEDULE_API_LOGIN, SCHEDULE_API_PASSWORD, SCHEDULE_API_URL
-try:
-    from ntlm import HTTPNtlmAuthHandler
-except ImportError:
-    print('There is no python-ntlm installed')
-    print('Install it by: pip install python-ntlm')
-    exit()
-
+from requests_ntlm import HttpNtlmAuth
 
 class Schedule:
     def __init__(self):
         self.logger = libs.logger.get_logger(__name__)
+        self.mifareSession = requests.Session()
+        self.mifareSession.auth = HttpNtlmAuth(secrets.MIFARE_API_LOGIN, secrets.MIFARE_API_PASSWORD,
+            self.mifareSession)
 
-    def requestSchedule(self, mid):
-        url = SCHEDULE_API_URL % mid
+        self.studentSession = requests.Session()
+        self.studentSession.auth = HttpNtlmAuth(secrets.STUDENT_API_LOGIN, secrets.STUDENT_API_PASSWORD,
+            self.studentSession)
 
-        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        passman.add_password(None, url, SCHEDULE_API_LOGIN, SCHEDULE_API_PASSWORD)
-        # create the NTLM authentication handler
-        authNTLM = HTTPNtlmAuthHandler.HTTPNtlmAuthHandler(passman)
-
-        # create and install the opener
-        opener = urllib2.build_opener(authNTLM)
-        urllib2.install_opener(opener)
-
-        response = []
+    def requestMifare(self, mid):
+        url = secrets.SCHEDULE_API_URL.format(mid)
+        schedule = []
         try:
-            resp = urllib2.urlopen(url)
-            if resp.getcode() == 200:
-                response = resp.read()
+            response = self.mifareSession.get(url)
+            if response.status_code == 200:
+                schedule = response.json()
             else:
-                self.logger.debug("[MifareUID: %d] HTTP return code %d \n%s" % (mid, resp.getcode(), resp.info()))
+                self.logger.error("[MifareUID: {}] HTTP return code {} \n{}".format(mid, response.status_code, response.text))
                 return []
-            # 400 - not found or processing error or something else went wrong
-        except urllib2.URLError as e:
+        except requests.exceptions.RequestException as e:
             # Where is the internet connection?
-            self.logger.debug("[MifareUID: %d] Except on connection \n%s" % (mid, e))
+            self.logger.error("[MifareUID: %d] Except on connection \n%s" % (mid, e))
             return []
 
-        scheduleObj = []
-        try:
-            scheduleObj = json.loads(response)
-        except:
-            self.logger.debug("[MifareUID: %d] Except on Parse \n%s" % (mid, response))
+        result = []
 
-        ret = []
-        for event in scheduleObj:
-            ev = {}
-            start = event["DataRoz"].split(" ")
+        for class_node in schedule:
+            class_object = {}
+            start = class_node["DataRoz"].split(" ")
             if datetime.strptime(start[0], "%Y-%m-%d").date() != date.today():
                 continue
 
-            end = event["DataZak"].split(" ")
+            end = class_node["DataZak"].split(" ")
             stime = start[1].split(":")
             etime = end[1].split(":")
 
-            ev["date"] = start[0]
-            ev["start"] = {'h': int(stime[0]), 'm': int(stime[1])}
-            ev["end"] = {'h': int(etime[0]), 'm': int(etime[1])}
-            ev["delta"] = (ev["end"]['h'] - ev["start"]['h']) * 60 + (ev["end"]['m'] - ev["start"]['m'])
-            ev["inrow"] = 1
-            ev['margin'] = 0
+            class_object["date"] = start[0]
+            class_object["start"] = {'h': int(stime[0]), 'm': int(stime[1])}
+            class_object["end"] = {'h': int(etime[0]), 'm': int(etime[1])}
 
-            for e in ret:
-                if (ev["start"]['h'] >= e["start"]['h']) and \
-                    (ev["end"]['h'] <= e["end"]['h']) and \
-                    (ev["date"] == e["date"]):
-                    ev['inrow'] += 1
-                    ev['margin'] += 1
-                    e['inrow'] += 1
-
-            if(event["Nazwa"].strip()):
-                ev["title"] = event["Nazwa"]
+            if(class_node["Nazwa"].strip()):
+                class_object["title"] = class_node["Nazwa"]
             else:
-                ev["title"] = event["Kod"]
-            ev["room"] = event["NazwaSali"]
+                class_object["title"] = class_node["Kod"]
 
-            ret.append(ev)
+            class_object["room"] = class_node["NazwaSali"]
 
-        if(len(ret) < 1):
-            self.logger.debug("[MifareUID: %d] Return empty schedule" % (mid,))
+            result.append(class_object)
+
+        if(len(result) < 1):
+            self.logger.debug("[MifareUID: {}] Return empty schedule".format(mid))
         else:
-            self.logger.debug("[MifareUID: %d] Probably successfully" % (mid,))
+            self.logger.debug("[MifareUID: {}] Probably successfully".format(mid))
 
-        return ret
+        return result
+
+    def requestStudent(self, studentId):
+        fromDate = toDate = date.today().isoformat()
+        url = secrets.STUDENT_API_URL.format(studentId, fromDate, toDate)
+
+        try:
+            response = self.studentSession.get(url)
+            if response.status_code == 200:
+                schedule = ET.fromstring(response.text)
+                result = []
+
+                for class_node in schedule:
+                    class_object = {}
+                    start = class_node.find("DataRoz").text.split(" ")
+                    end = class_node.find("DataZak").text.split(" ")
+                    stime = start[1].split(":")
+                    etime = end[1].split(":")
+
+                    class_object["date"] = start[0]
+                    class_object["start"] = {'h': int(stime[0]), 'm': int(stime[1])}
+                    class_object["end"] = {'h': int(etime[0]), 'm': int(etime[1])}
+
+                    if(class_node.find("Nazwa").text.strip()):
+                        class_object["title"] = class_node.find("Nazwa").text
+                    else:
+                        class_object["title"] = class_node.find("Kod").text
+
+                    class_object["room"] = class_node.find("NazwaSali").text
+
+                    result.append(class_object)
+
+            else:
+                self.logger.error("[studentId: {}] HTTP return code {} \n{}".format(studentId, response.status_code, response.text))
+                return []
+        except requests.exceptions.RequestException as e:
+            self.logger.error("[studentId: {}] Exception on connection \n{}".format(studentId, e))
+            return []
